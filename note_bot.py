de0815132c2a5195e8bedf57be35d23d3b90b551
mr-elvis-vis <elvis2025@yandex.ru>
2022-04-23 11:30:43 +0200

    class Requester added. processors structure fixed

--- note_bot.py
+++ note_bot.py
@@ -5,11 +5,56 @@ from dotenv import load_dotenv
 from telegram import ReplyKeyboardMarkup
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 
-from bot_date import DICT_URL, FIRST_NOTE
+from bot_date import DICT_URL, FIRST_NOTE, HELP_TEXT
 
 load_dotenv()
 
 token = os.getenv('TOKEN')
+
+
+class Requester():
+    def get_list(self, chat):
+        b_token = self.token(chat)
+        response = requests.get(DICT_URL['note_list'], headers={
+            "Authorization": f"Bearer {b_token}"
+        })
+        return response
+
+    def get_note(self, chat, note_id):
+        url = DICT_URL['note_create']
+        url_note = f'{url}{note_id}'
+        b_token = self.token(chat)
+        response = requests.get(url_note, headers={
+            "Authorization": f"Bearer {b_token}"
+        })
+        return response
+
+    def delete_note(self, chat, note_id):
+        b_token = self.token(chat)
+        url = DICT_URL['note_create']
+        url_note = f'{url}{note_id}'
+        requests.delete(url_note, headers={
+            "Authorization": f"Bearer {b_token}"
+        })
+
+    def create_note(self, chat, mes_text):
+        b_token = self.token(chat)
+        response = requests.post(DICT_URL['note_create'], headers={
+            "Authorization": f"Bearer {b_token}"
+        }, data={
+            "text": f"{mes_text}",
+        })
+        return response
+
+    def token(self, chat):
+        with open("token.txt", 'r+') as file:
+            d = {}
+            lines = file.readlines()
+            for line in lines:
+                key, value = line.split()
+                d[key] = value
+            b_token = d[str(chat.id)]
+        return b_token
 
 
 def api_registration(update, context):
@@ -33,10 +78,11 @@ def api_registration(update, context):
             text='Пользователь успешно создан!',
             reply_markup=button
         )
-        note_create(update, context)
+        note_create(update, context, chat, FIRST_NOTE, name)
         note_list_detail(update, context)
     else:
-        context.bot.send_message(chat.id, 'Упс! Что-то не так!')
+        context.bot.send_message(chat.id, 'Упс! Что-то не так! Может вы уже '
+                                          'имеете список заметок?')
 
 
 def refresh_token(update, context):
@@ -62,18 +108,24 @@ def refresh_token(update, context):
     )
 
 
-def note_list_detail(update, context):
+def note_processor(update, context):
     chat = update.effective_chat
-    with open("token.txt", 'r+') as file:
-        d = {}
-        lines = file.readlines()
-        for line in lines:
-            key, value = line.split()
-            d[key] = value
-        b_token = d[str(chat.id)]
-        response = requests.get(DICT_URL['note_list'], headers={
-            "Authorization": f"Bearer {b_token}"
-        })
+    name = update.message.chat.first_name
+    mes_text = update.message.text
+    mes_text_spl = mes_text.split()
+    if mes_text_spl[0] == 'delete' and len(mes_text_spl) == 2:
+        note_del(update, context, mes_text_spl[1])
+    elif mes_text.isdigit():
+        note_id = int(update.message.text)
+        note_detail(update, context, note_id)
+    else:
+        note_create(update, context, chat, mes_text, name)
+
+
+def note_list_detail(update, context):
+    request = Requester()
+    chat = update.effective_chat
+    response = request.get_list(chat)
     response = response.json()
     message = ''
     for i in response:
@@ -94,136 +146,12 @@ def note_list_detail(update, context):
     )
 
 
-def note_create(update, context):
-    chat = update.effective_chat
-    name = update.message.chat.first_name
-    mes_text = update.message.text
-    mes_text_spl = mes_text.split()
-    if mes_text == '/start':
-        mess = FIRST_NOTE
-        with open("token.txt", 'r+') as file:
-            d = {}
-            lines = file.readlines()
-            for line in lines:
-                key, value = line.split()
-                d[key] = value
-            b_token = d[str(chat.id)]
-            response = requests.post(DICT_URL['note_create'], headers={
-                "Authorization": f"Bearer {b_token}"
-            }, data={
-                "text": f"{mess}",
-            })
-    elif mes_text_spl[0] == 'DEL' and len(mes_text_spl) == 2:
-        note_del(update, context, mes_text_spl[1])
-    else:
-        try:
-            note_id = int(update.message.text)
-            note_detail(update, context, note_id)
-        except:
-            mess = update.message.text
-            with open("token.txt", 'r+') as file:
-                d = {}
-                lines = file.readlines()
-                for line in lines:
-                    key, value = line.split()
-                    d[key] = value
-                b_token = d[str(chat.id)]
-                response = requests.post(DICT_URL['note_create'], headers={
-                    "Authorization": f"Bearer {b_token}"
-                }, data={
-                    "text": f"{mess}",
-                })
-
-            button = ReplyKeyboardMarkup([
-                ['/note_list'],
-                ['/refresh_token'], ['/help']
-            ],
-                resize_keyboard=True)
-            if response.status_code == 201:
-                context.bot.send_message(
-                    chat_id=chat.id,
-                    text=f'{name}! Заметка создана!',
-                    reply_markup=button
-                )
-            else:
-                context.bot.send_message(
-                    chat.id,
-                    'Упс! Что-то не так!',
-                    reply_markup=button
-                )
-                try:
-                    note_list_detail(update, context)
-                except RuntimeError:
-                    pass
-
-
 def note_detail(update, context, note_id):
+    request = Requester()
     chat = update.effective_chat
-    with open("token.txt", 'r+') as file:
-        d = {}
-        lines = file.readlines()
-        for line in lines:
-            key, value = line.split()
-            d[key] = value
-        b_token = d[str(chat.id)]
-        url = DICT_URL['note_create']
-        url_note = f'{url}{note_id}'
-        response = requests.get(url_note, headers={
-            "Authorization": f"Bearer {b_token}"
-        })
-        response = response.json()
-        message = response.get('text')
-        name = update.message.chat.first_name
-        button = ReplyKeyboardMarkup([
-            ['/note_list'],
-            ['/refresh_token'], ['/help']
-        ],
-            resize_keyboard=True)
-        context.bot.send_message(
-            chat_id=chat.id,
-            text=f'Заметка {note_id}: \n {message}',
-            reply_markup=button
-        )
-
-
-def note_del(update, context, mes_text):
-    chat = update.effective_chat
-    try:
-        note_id = int(mes_text)
-        with open("token.txt", 'r+') as file:
-            d = {}
-            lines = file.readlines()
-            for line in lines:
-                key, value = line.split()
-                d[key] = value
-            b_token = d[str(chat.id)]
-            url = DICT_URL['note_create']
-            url_note = f'{url}{note_id}'
-            requests.delete(url_note, headers={
-                "Authorization": f"Bearer {b_token}"
-            })
-            button = ReplyKeyboardMarkup([
-                ['/note_list'],
-                ['/refresh_token'], ['/help']
-            ],
-                resize_keyboard=True)
-            context.bot.send_message(
-                chat_id=chat.id,
-                text=f'Заметка {note_id} удалена',
-                reply_markup=button
-            )
-            note_list_detail(update, context)
-    except:
-        context.bot.send_message(
-            chat_id=chat.id,
-            text='Введите корректный номер заметки для удаления!',
-            reply_markup=button
-        )
-
-
-def help(update, context):
-    chat = update.effective_chat
-    name = update.message.chat.first_name
+    response = request.get_note(chat, note_id)
+    response = response.json()
+    message = response.get('text')
     button = ReplyKeyboardMarkup([
         ['/note_list'],
         ['/refresh_token'], ['/help']
@@ -231,16 +159,74 @@ def help(update, context):
         resize_keyboard=True)
     context.bot.send_message(
         chat_id=chat.id,
-        text=(
-            f'Привет, {name}!\nСписок команд бота:\nstart - инициализирует '
-            f'тебя как пользователя для дальнейшей работы\nhelp - ты сейчас '
-            f'здесь! =)\nrefresh_token - если после работы с ботом возникли '
-            f'какие-то проблемы, попробуй попросить бота обновить токен '
-            f'доступа\nnote_list - выдаст список всех твоих заметок\n '
-            f'~число~ введи номер заметки, чтобы посмотреть её подробно\n '
-            f'DEL ~число~ для удаления заметки **вводи ID заметки!\nПросто '
-            f'отправь текст этому боту и он создаст новую заметку!'
-        ),
+        text=f'Заметка {note_id}: \n {message}',
+        reply_markup=button
+    )
+
+
+def note_del(update, context, mes_text):
+    chat = update.effective_chat
+    note_id = int(mes_text)
+    try:
+        request = Requester()
+        request.delete_note(chat, note_id)
+        button = ReplyKeyboardMarkup([
+            ['/note_list'],
+            ['/refresh_token'], ['/help']
+        ],
+            resize_keyboard=True)
+        context.bot.send_message(
+            chat_id=chat.id,
+            text=f'Заметка {note_id} удалена',
+            reply_markup=button
+        )
+        note_list_detail(update, context)
+    except:
+        button = ReplyKeyboardMarkup([
+            ['/note_list'],
+            ['/refresh_token'], ['/help']
+        ],
+            resize_keyboard=True)
+        context.bot.send_message(
+            chat_id=chat.id,
+            text='Введите корректный номер заметки для удаления!',
+            reply_markup=button
+        )
+
+
+def note_create(update, context, chat, mes_text, name):
+    request = Requester()
+    response = request.create_note(chat, mes_text)
+    button = ReplyKeyboardMarkup([
+        ['/note_list'],
+        ['/refresh_token'], ['/help']
+    ],
+        resize_keyboard=True)
+    if response.status_code == 201:
+        context.bot.send_message(
+            chat_id=chat.id,
+            text=f'{name}! Заметка создана!',
+            reply_markup=button
+        )
+    else:
+        context.bot.send_message(
+            chat.id,
+            'Упс! Что-то не так!',
+            reply_markup=button
+        )
+        note_list_detail(update, context)
+
+
+def help(update, context):
+    chat = update.effective_chat
+    button = ReplyKeyboardMarkup([
+        ['/note_list'],
+        ['/refresh_token'], ['/help']
+    ],
+        resize_keyboard=True)
+    context.bot.send_message(
+        chat_id=chat.id,
+        text=HELP_TEXT,
         reply_markup=button
     )
 
@@ -257,7 +243,7 @@ def main():
     updater.dispatcher.add_handler(CommandHandler('note_list',
                                                   note_list_detail))
     updater.dispatcher.add_handler(MessageHandler(Filters.text,
-                                                  note_create))
+                                                  note_processor))
 
     updater.start_polling()
     updater.idle()
